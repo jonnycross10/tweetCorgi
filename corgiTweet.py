ee8a0571dcce5cb211f28907344804a6607eb9f8
jonnycross10 <crossjonny5@gmail.com>
2020-09-24 22:26:21 -0700

    I can now send the picture in the tweet

--- corgiTweet.py
+++ corgiTweet.py
@@ -1,5 +1,14 @@
 import requests
 import json
+import tweepy
+from secrets import consumer_key,consumer_secret, access_token, access_token_secret
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
 
 
 randurl = "https://dog.ceo/api/breed/pembroke/images/random"
@@ -7,4 +16,6 @@ randurl = "https://dog.ceo/api/breed/pembroke/images/random"
 r = requests.get(randurl)
 
 img = json.loads(r.text)
-print(img['message'])
+#print(img['message'])
+
+api.update_status(status = "WUV U @errmalou69 " + img['message'])
