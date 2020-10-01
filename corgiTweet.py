6b1c1b0e9b5e8f742dc754c4e3252b564d909d24
jonnycross10 <crossjonny5@gmail.com>
2020-09-30 22:59:44 -0700

    Program now runs once at the same time every day

--- corgiTweet.py
+++ corgiTweet.py
@@ -1,8 +1,8 @@
+from secrets import consumer_key,consumer_secret, access_token, access_token_secret
+from datetime import datetime
 import requests
 import json
 import tweepy
-from secrets import consumer_key,consumer_secret, access_token, access_token_secret
-
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -10,12 +10,27 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
+x = True
+counter = 0 #only want to run the first if statement once
+while(x):
+	x=datetime.now()
+	h = x.strftime("%H")
+	m = x.strftime("%M")
+	s = x.strftime("%S")
+	if (h =='08' and m == '00' and s =='00' and counter ==0):
+		
+		randurl = "https://dog.ceo/api/breed/pembroke/images/random"
 
-randurl = "https://dog.ceo/api/breed/pembroke/images/random"
+		r = requests.get(randurl)
 
-r = requests.get(randurl)
+		img = json.loads(r.text)
 
-img = json.loads(r.text)
-#print(img['message'])
-
-api.update_status(status = "WUV U @errmalou69 " + img['message'])
+		api.update_status(status = "Good morning wuv @errmalou69 " + img['message'])
+		
+		counter = 1
+	if (h =='08' and m == '01' and s =='00'):
+		counter = 0
+		#this will make it so after the minute has elapsed the counter is reset
+		#for the next day
+		
+		
